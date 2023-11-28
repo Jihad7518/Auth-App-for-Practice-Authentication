58fb2a26ff85890e2325b63ff9d3137fd1ca2626
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-29 00:31:04 +0600

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -89,3 +89,29 @@ exports.login = async (req,res) => {
                                 {
                                     expiresIn:"2h",
                                 });
+
+            user = user.toObject();
+            user.token = token;
+            user.password = undefined;
+
+            const options = {
+                expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly:true,
+            }
+
+            res.cookie("babbarCookie", token, options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:'User Logged in successfully',
+            });
+        }
+        else {
+            //passwsord do not match
+            return res.status(403).json({
+                success:false,
+                message:"Password Incorrect",
+            });
+        }
+
+    }
